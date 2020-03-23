02a6f7fe648df34cdbd0c0b8838ef658c5023354
Rubén Ostos <reflejodevapor@gmail.com>
2020-03-23 00:04:25 -0600

    kk

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 0.0f;
+    bool currentlyDashing = false;
+    public enum CurrentGun
+    {
+        red,
+        green,
+        blue
+    }
+
+    public CurrentGun Color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var Horizontal = Input.GetAxis("Horizontal");
+        var Vertical = Input.GetAxis("Vertical");
+
+        var GunR = Input.GetButtonDown("GunR");
+        var GunG = Input.GetButtonDown("GunG");
+        var GunB = Input.GetButtonDown("GunB");
+
+        var Dash = Input.GetButtonDown("Dash");
+
+        this.gameObject.transform.Translate(new Vector2(Horizontal, Vertical) * playerSpeed * Time.deltaTime, Space.World);
+
+        if (GunR && Color != CurrentGun.red)
+        {
+            Debug.Log("GunR");
+            Color = CurrentGun.red;
+        }
+        else if (GunG && Color != CurrentGun.green)
+        {
+            Debug.Log("GunG");
+            Color = CurrentGun.green;
+        }
+        else if (GunB && Color != CurrentGun.blue)
+        {
+            Debug.Log("GunB");
+            Color = CurrentGun.blue;
+        }
+
+        if (Dash && currentlyDashing == false && !(Horizontal == 0 && Vertical == 0))
+        {
+            StartCoroutine(Dashing());
+        }
+
+        
+        Vector3 mouseWorldSpace = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.LookAt(mouseWorldSpace,Vector3.forward);
+
+    }
+
+    IEnumerator Dashing()
+    {
+        Debug.Log("Dash Started");
+        currentlyDashing = true;
+        playerSpeed *= 3.1f;
+        yield return new WaitForSeconds(0.2f);
+        playerSpeed /= 3.1f;
+        currentlyDashing = false;
+        Debug.Log("Dash Finished");
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public AnimationCurve BeatAnimation;
+    public float maxTime = 0.0f;
+    public float chrono = 0.0f;
+    public float BPM = 0.0f;
+    public PlayerController playerController;
+
+    public Image BPMBorder;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        maxTime = 60 / BPM;
+
+        chrono += Time.deltaTime;
+        if(chrono > maxTime)
+        {
+            chrono = 0;
+        }
+
+        float alpha = BeatAnimation.Evaluate(chrono);
+        BPMBorder.color = new Color(BPMBorder.color.r, BPMBorder.color.g, BPMBorder.color.b, alpha);
+
+        switch (playerController.Color)
+        {
+            case PlayerController.CurrentGun.red:
+                BPMBorder.color = new Color(Color.red.r, Color.red.g, Color.red.b, alpha);
+                break;
+            case PlayerController.CurrentGun.green:
+                BPMBorder.color = new Color(Color.green.r, Color.green.g, Color.green.b, alpha);
+                break;
+            case PlayerController.CurrentGun.blue:
+                BPMBorder.color = new Color(Color.blue.r, Color.blue.g, Color.blue.b, alpha);
+                break;
+
+
+        }
+
+
+    }
+}
